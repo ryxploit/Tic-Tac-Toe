ff158c912bc34cf2adb4793a2febb771039411b9
Carlos leon <karlos_eb1@hotmail.com>
2017-02-13 08:24:57 -0600

    beta 0.1

---
+++ Tic Tac Toe/Tic Tac Toe/Form1.Designer.vb
@@ -0,0 +1,304 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.White
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(121, 80)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 100)
+        Me.Button1.TabIndex = 0
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.White
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(230, 80)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 100)
+        Me.Button2.TabIndex = 1
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.White
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(336, 80)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(100, 100)
+        Me.Button3.TabIndex = 2
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.White
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(121, 186)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(100, 100)
+        Me.Button4.TabIndex = 3
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.White
+        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(230, 186)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(100, 100)
+        Me.Button5.TabIndex = 4
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.Color.White
+        Me.Button6.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.Location = New System.Drawing.Point(336, 186)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(100, 100)
+        Me.Button6.TabIndex = 5
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Button7
+        '
+        Me.Button7.BackColor = System.Drawing.Color.White
+        Me.Button7.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button7.Location = New System.Drawing.Point(121, 292)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(100, 100)
+        Me.Button7.TabIndex = 6
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'Button8
+        '
+        Me.Button8.BackColor = System.Drawing.Color.White
+        Me.Button8.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button8.Location = New System.Drawing.Point(230, 292)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(100, 100)
+        Me.Button8.TabIndex = 7
+        Me.Button8.UseVisualStyleBackColor = False
+        '
+        'Button9
+        '
+        Me.Button9.BackColor = System.Drawing.Color.White
+        Me.Button9.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button9.Location = New System.Drawing.Point(336, 292)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(100, 100)
+        Me.Button9.TabIndex = 8
+        Me.Button9.UseVisualStyleBackColor = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.Transparent
+        Me.GroupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.GroupBox1.Controls.Add(Me.Button10)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(34, 441)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(542, 100)
+        Me.GroupBox1.TabIndex = 9
+        Me.GroupBox1.TabStop = False
+        '
+        'Button10
+        '
+        Me.Button10.BackColor = System.Drawing.Color.White
+        Me.Button10.BackgroundImage = CType(resources.GetObject("Button10.BackgroundImage"), System.Drawing.Image)
+        Me.Button10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.Button10.Location = New System.Drawing.Point(440, 19)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(80, 63)
+        Me.Button10.TabIndex = 19
+        Me.Button10.UseVisualStyleBackColor = False
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.BackColor = System.Drawing.Color.White
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(295, 58)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(0, 24)
+        Me.Label7.TabIndex = 18
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.BackColor = System.Drawing.Color.White
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(269, 16)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(73, 25)
+        Me.Label6.TabIndex = 17
+        Me.Label6.Text = "Turno"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.White
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(147, 62)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(19, 20)
+        Me.Label4.TabIndex = 16
+        Me.Label4.Text = "0"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.White
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(147, 24)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(19, 20)
+        Me.Label3.TabIndex = 15
+        Me.Label3.Text = "0"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.White
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(92, 57)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 25)
+        Me.Label2.TabIndex = 14
+        Me.Label2.Text = "O"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.White
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(94, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(27, 25)
+        Me.Label1.TabIndex = 13
+        Me.Label1.Text = "X"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.Color.Transparent
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.White
+        Me.Label5.Location = New System.Drawing.Point(121, 27)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(315, 37)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = " .l.     {  X . O  }    .l."
+        '
+        'Button11
+        '
+        Me.Button11.BackColor = System.Drawing.Color.White
+        Me.Button11.Location = New System.Drawing.Point(34, 420)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(96, 25)
+        Me.Button11.TabIndex = 11
+        Me.Button11.Text = "Acerca de..."
+        Me.Button11.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(600, 553)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Tic Tac Toe Beta 0.1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Button11 As Button
+End Class
---
+++ Tic Tac Toe/Tic Tac Toe/Form1.vb
@@ -0,0 +1,315 @@
+﻿Public Class Form1
+
+    Dim turno As Integer
+
+    Private Sub ganar()
+        Dim i As Integer
+
+        Try
+            'X
+            If i = 1 Then
+
+            ElseIf Button1.Text = "X" And Button2.Text = "X" And Button3.Text = "X" Then
+                MsgBox("El jugador X ha ganado")
+                Label3.Text += 1
+                Call desabilitarbtn()
+
+            ElseIf Button1.Text = "X" And Button4.Text = "X" And Button7.Text = "X" Then
+                MsgBox("El jugador X ha ganado")
+                Label3.Text += 1
+                Call desabilitarbtn()
+
+
+            ElseIf Button2.Text = "X" And Button5.Text = "X" And Button8.Text = "X" Then
+                MsgBox("El jugador X ha ganado")
+                Label3.Text += 1
+                Call desabilitarbtn()
+
+            ElseIf Button3.Text = "X" And Button6.Text = "X" And Button9.Text = "X" Then
+                MsgBox("El jugador X ha ganado")
+                Label3.Text += 1
+                Call desabilitarbtn()
+
+            ElseIf Button3.Text = "X" And Button5.Text = "X" And Button7.Text = "X" Then
+                MsgBox("El jugador X ha ganado")
+                Label3.Text += 1
+                Call desabilitarbtn()
+
+            ElseIf Button1.Text = "X" And Button5.Text = "X" And Button9.Text = "X" Then
+                MsgBox("El jugador X ha ganado")
+                Label3.Text += 1
+                Call desabilitarbtn()
+
+            ElseIf Button7.Text = "X" And Button8.Text = "X" And Button9.Text = "X" Then
+                MsgBox("El jugador X ha ganado")
+                Label3.Text += 1
+                Call desabilitarbtn()
+
+            ElseIf Button4.Text = "X" And Button5.Text = "X" And Button6.Text = "X" Then
+                MsgBox("El jugador X ha ganado")
+                Label3.Text += 1
+                Call desabilitarbtn()
+
+                'O
+
+            ElseIf Button1.Text = "O" And Button2.Text = "O" And Button3.Text = "O" Then
+                MsgBox("El jugador O ha ganado")
+                Label4.Text += 1
+                Call desabilitarbtn()
+
+            ElseIf Button1.Text = "O" And Button4.Text = "O" And Button7.Text = "O" Then
+                MsgBox("El jugador O ha ganado")
+                Label4.Text += 1
+                Call desabilitarbtn()
+
+            ElseIf Button2.Text = "O" And Button5.Text = "O" And Button8.Text = "O" Then
+                MsgBox("El jugador O ha ganado")
+                Label4.Text += 1
+                Call desabilitarbtn()
+
+            ElseIf Button3.Text = "O" And Button6.Text = "O" And Button9.Text = "O" Then
+                MsgBox("El jugador O ha ganado")
+                Label4.Text += 1
+                Call desabilitarbtn()
+
+            ElseIf Button3.Text = "O" And Button5.Text = "O" And Button7.Text = "O" Then
+                MsgBox("El jugador O ha ganado")
+                Label4.Text += 1
+                Call desabilitarbtn()
+
+            ElseIf Button1.Text = "O" And Button5.Text = "O" And Button9.Text = "O" Then
+                MsgBox("El jugador O ha ganado")
+                Label4.Text += 1
+                Call desabilitarbtn()
+
+            ElseIf Button7.Text = "O" And Button8.Text = "O" And Button9.Text = "O" Then
+                MsgBox("El jugador O ha ganado")
+                Label4.Text += 1
+                Call desabilitarbtn()
+
+            ElseIf Button4.Text = "O" And Button5.Text = "O" And Button6.Text = "O" Then
+                MsgBox("El jugador O ha ganado")
+                Label4.Text += 1
+                Call desabilitarbtn()
+
+
+
+            End If
+
+        Catch ex As Exception
+            MsgBox("fallo metodo ganar")
+        End Try
+
+
+    End Sub
+
+    Private Sub desabilitarbtn()
+        Button1.Enabled = (False)
+        Button2.Enabled = (False)
+        Button3.Enabled = (False)
+        Button4.Enabled = (False)
+        Button5.Enabled = (False)
+        Button6.Enabled = (False)
+        Button7.Enabled = (False)
+        Button8.Enabled = (False)
+        Button9.Enabled = (False)
+
+    End Sub
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If turno = 1 Then
+            Button1.Text = "O"
+            Label7.Text = "X"
+        Else
+            Button1.Text = "X"
+            Label7.Text = "O"
+        End If
+
+        turno += 1
+        If turno > 2 Then
+            turno = 1
+        End If
+        Call ganar()
+        Button1.Enabled = (False)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If turno = 1 Then
+            Button2.Text = "O"
+            Label7.Text = "X"
+        Else
+            Button2.Text = "X"
+            Label7.Text = "O"
+        End If
+
+        turno += 1
+        If turno > 2 Then
+            turno = 1
+        End If
+        Call ganar()
+        Button2.Enabled = (False)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If turno = 1 Then
+            Button3.Text = "O"
+            Label7.Text = "X"
+        Else
+            Button3.Text = "X"
+            Label7.Text = "O"
+        End If
+
+        turno += 1
+        If turno > 2 Then
+            turno = 1
+        End If
+        Call ganar()
+        Button3.Enabled = (False)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If turno = 1 Then
+            Button4.Text = "O"
+            Label7.Text = "X"
+        Else
+            Button4.Text = "X"
+            Label7.Text = "O"
+        End If
+
+        turno += 1
+        If turno > 2 Then
+            turno = 1
+        End If
+        Call ganar()
+        Button4.Enabled = (False)
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If turno = 1 Then
+            Button5.Text = "O"
+            Label7.Text = "X"
+        Else
+            Button5.Text = "X"
+            Label7.Text = "O"
+        End If
+
+        turno += 1
+        If turno > 2 Then
+            turno = 1
+        End If
+        Call ganar()
+        Button5.Enabled = (False)
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        If turno = 1 Then
+            Button6.Text = "O"
+            Label7.Text = "X"
+        Else
+            Button6.Text = "X"
+            Label7.Text = "O"
+        End If
+
+        turno += 1
+        If turno > 2 Then
+            turno = 1
+        End If
+        Call ganar()
+        Button6.Enabled = (False)
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        If turno = 1 Then
+            Button7.Text = "O"
+            Label7.Text = "X"
+        Else
+            Button7.Text = "X"
+            Label7.Text = "O"
+        End If
+
+        turno += 1
+        If turno > 2 Then
+            turno = 1
+        End If
+        Call ganar()
+        Button7.Enabled = (False)
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        If turno = 1 Then
+            Button8.Text = "O"
+            Label7.Text = "X"
+        Else
+            Button8.Text = "X"
+            Label7.Text = "O"
+        End If
+
+        turno += 1
+        If turno > 2 Then
+            turno = 1
+        End If
+        Call ganar()
+        Button8.Enabled = (False)
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        If turno = 1 Then
+            Button9.Text = "O"
+            Label7.Text = "X"
+        Else
+            Button9.Text = "X"
+            Label7.Text = "O"
+        End If
+
+        turno += 1
+        If turno > 2 Then
+            turno = 1
+        End If
+        Call ganar()
+        Button9.Enabled = (False)
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        Button1.Text = ""
+        Button1.Enabled = (True)
+
+        Button2.Text = ""
+        Button2.Enabled = (True)
+
+        Button3.Text = ""
+        Button3.Enabled = (True)
+
+        Button4.Text = ""
+        Button4.Enabled = (True)
+
+        Button5.Text = ""
+        Button5.Enabled = (True)
+
+        Button6.Text = ""
+        Button6.Enabled = (True)
+
+        Button7.Text = ""
+        Button7.Enabled = (True)
+
+        Button8.Text = ""
+        Button8.Enabled = (True)
+
+        Button9.Text = ""
+        Button9.Enabled = (True)
+
+        Label7.Text = ""
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        MsgBox("Hecho por juan carlos leon salcido")
+    End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+End Class
